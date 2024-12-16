0a4848df199e2034d3548c866903cdde1b5d5ada
shriananda <shriananda.s05@gmail.com>
2024-12-16 23:04:24 +0530

    first

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void method1()
+	{
+		System.out.println("method1");
+	}
+
+	@Test
+	public void method2()
+	{
+		System.out.println("method2");
+	}
+	
+}
