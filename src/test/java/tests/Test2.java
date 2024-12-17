a42e3601dba76c7825f2586a5c15eb34e64ca6a6
shriananda <shriananda.s05@gmail.com>
2024-12-17 11:08:55 +0530

    first

---
+++ src/test/java/tests/Test2.java
@@ -0,0 +1,21 @@
+package tests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	static WebDriver driver= new ChromeDriver();
+	@Test
+	public void method1()
+	{
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.get("https://www.google.com/");
+		driver.close();
+	}
+	
+}
